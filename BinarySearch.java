2a2f6a68c7860c9f57e5d6be25557d3eb5f9eb47
iammohit07 <65330953+iammohit07@users.noreply.github.com>
2021-07-18 17:40:30 +0530

    Binary Search

---
+++ BinarySearch.java
@@ -0,0 +1,102 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinarySearch
+{
+    public static int searchItemByIteration(int item, int[] arr)
+    {
+        int low = 0,high = arr.length - 1;
+        while (low<=high)
+        {
+            int mid = (low+high)/2;
+            if(arr[mid] == item)
+            {
+                return mid;
+            }
+            else if (item>arr[mid])
+            {
+                //element is in right side
+                low = mid + 1;
+            }
+            else if (item<arr[mid])
+            {
+                //element is in left side
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int searchItemRecursively(int item, int[] arr,int low,int high)
+    {
+        if (low > high)
+        {
+            return -1;
+        }
+        int mid = (low + high)/2;
+        if (arr[mid] == item)
+        {
+            return mid;
+        }
+        if (item>arr[mid])
+        {
+            //element is in right side
+            return searchItemRecursively(item,arr,mid + 1,high);
+        }
+        if (item<arr[mid])
+        {
+            //element is in left side
+            return searchItemRecursively(item,arr,low,mid-1);
+        }
+        return -1;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.print("Enter the Size of Array : ");
+        Scanner scanner = new Scanner(System.in);
+        int size_array = scanner.nextInt();
+
+        int[] arr = new int[size_array];
+        System.out.println("Enter the Array : ");
+
+        for(int i = 0;i<size_array;i++)
+        {
+            arr[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println("Sorted  array is : ");
+        for (int s : arr)
+        {
+            System.out.print(s);
+        }
+
+        System.out.println("Enter element to search from array : ");
+        int x = scanner.nextInt();
+
+        /*By Iteration
+        int position = searchItemByIteration(x, arr);
+        if (position == -1)
+        {
+            System.out.println("Element isn't present in array !!");
+        }
+        else
+        {
+            System.out.println("Element is present at index: " + position + ", Element is : " + arr[position] );
+        }*/
+
+        //By Recursion
+        int low = 0,high = arr.length-1;
+        int position = searchItemRecursively(x, arr,low,high);
+        if (position == -1)
+        {
+            System.out.println("Element isn't present in array !!");
+        }
+        else
+        {
+            System.out.println("Element is present at index: " + position + ", Element is : " + arr[position] );
+        }
+    }
+}
